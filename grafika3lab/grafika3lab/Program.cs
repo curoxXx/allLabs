895bbd02d0c61ce3f78e15c980809b2ebee5880a
curoxXx <153936207+curoxXx@users.noreply.github.com>
2024-10-29 18:38:40 +0300

    lab2, lab3 added

---
+++ grafika3lab/grafika3lab/Program.cs
@@ -0,0 +1,69 @@
+﻿using grafika3lab.Properties;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Remoting.Channels;
+
+class ImageProcessing
+{
+    public static Bitmap ConvertToBlackAndWhite(Bitmap image)
+    {
+        Bitmap result = new Bitmap(image.Width, image.Height);
+
+        for (int x = 0; x < image.Width; x++)
+        {
+            for (int y = 0; y < image.Height; y++)
+            {
+                Color pixelColor = image.GetPixel(x, y);
+                float saturation = GetSaturation(pixelColor);
+
+                if (saturation < 0.75f)
+                {
+                    int grayValue = (int)(pixelColor.R * 0.299 + pixelColor.G * 0.587 + pixelColor.B * 0.114);
+                    Color grayColor = Color.FromArgb(grayValue, grayValue, grayValue);
+                    result.SetPixel(x, y, grayColor);
+                }
+                else
+                {
+                    result.SetPixel(x, y, pixelColor);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static float GetSaturation(Color color)
+    {
+        float max = Math.Max(color.R, Math.Max(color.G, color.B));
+        float min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+        if (max == 0) return 0;
+        return 1f - (1f * min / max);
+    }
+
+    public static Bitmap OverlayImages(Bitmap baseImage, Bitmap overlayImage)
+    {
+        Bitmap result = new Bitmap(baseImage.Width, baseImage.Height);
+
+        using (Graphics g = Graphics.FromImage(result))
+        {
+            g.DrawImage(baseImage, 0, 0);
+            g.DrawImage(overlayImage, 0, 0);
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        Bitmap image1 = Resources.image1;
+        Bitmap image2 = Resources.image2;
+        Bitmap processedImage1 = ConvertToBlackAndWhite(image1);
+        Bitmap processedImage2 = ConvertToBlackAndWhite(image2);
+        processedImage1.Save("output1.png", ImageFormat.Png);
+        processedImage2.Save("output2.png", ImageFormat.Png);
+        Bitmap finalImage = OverlayImages(processedImage1, processedImage2);
+        finalImage.Save("final_output.png", ImageFormat.Png);
+        Console.WriteLine("Изображения успешно обработаны и сохранены.");
+    }
+}
---
+++ laba2Grafik/laba2Grafik/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2Grafik
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox pictureBox;
+        private Button drawButton;
+        private TextBox radiusTextBox;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox = new PictureBox
+            {
+                Size = new Size(400, 400),
+                BorderStyle = BorderStyle.Fixed3D,
+                Location = new Point(10, 10)
+            };
+
+            drawButton = new Button
+            {
+                Text = "Нарисовать окружность",
+                Location = new Point(10, 420)
+            };
+            drawButton.Click += button1_Click;
+            drawButton.Width = 200;
+            radiusTextBox = new TextBox
+            {
+                Location = new Point(220, 420),
+                Width = 50
+            };
+
+            Controls.Add(pictureBox);
+            Controls.Add(drawButton);
+            Controls.Add(radiusTextBox);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int radius;
+            if (int.TryParse(radiusTextBox.Text, out radius))
+            {
+                Bitmap bmp = new Bitmap(pictureBox.Width, pictureBox.Height);
+                DrawCircle(bmp, pictureBox.Width / 2, pictureBox.Height / 2, radius);
+                pictureBox.Image = bmp;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректный радиус.");
+            }
+        }
+        private void DrawCircle(Bitmap bmp, int X1, int Y1, int R)
+        {
+            int x = 0;
+            int y = R;
+            int delta = 1 - 2 * R;
+            int error = 0;
+
+            while (y >= x)
+            {
+                DrawPixel(bmp, X1 + x, Y1 + y);
+                DrawPixel(bmp, X1 + x, Y1 - y);
+                DrawPixel(bmp, X1 - x, Y1 + y);
+                DrawPixel(bmp, X1 - x, Y1 - y);
+                DrawPixel(bmp, X1 + y, Y1 + x);
+                DrawPixel(bmp, X1 + y, Y1 - x);
+                DrawPixel(bmp, X1 - y, Y1 + x);
+                DrawPixel(bmp, X1 - y, Y1 - x);
+
+                error = 2 * (delta + y) - 1;
+
+                if ((delta < 0) && (error <= 0))
+                {
+                    delta += 2 * ++x + 1;
+                    continue;
+                }
+                if ((delta > 0) && (error > 0))
+                {
+                    delta -= 2 * --y + 1;
+                    continue;
+                }
+                delta += 2 * (++x - --y);
+            }
+        }
+        private void DrawPixel(Bitmap bmp, int x, int y)
+        {
+            if (x >= 0 && x < bmp.Width && y >= 0 && y < bmp.Height)
+            {
+                bmp.SetPixel(x, y, Color.Black);
+            }
+        }
+    }
+}
